156a782ad68099bcf4500b168610de90d4855f29
jaykumawat-ai <jaykumawat1201@gmail.com>
2025-07-24 09:07:55 +0530

    third comit

--- funtion.js
+++ funtion.js
@@ -23,12 +23,53 @@
 // console.log(addNum(10,20));
 
  
-function addNum(num1, num2) {
-    let n1 = Number(num1);
-    let n2 = Number(num2);
-    if (isNaN(n1)||isNaN(n2)){
-        return("enter proper number")
-    }
-    return n1 + n2;
-}
-console.log(addNum(10, "abc"));//nan-> not a number
+// function addNum(num1, num2) {
+//     let n1 = Number(num1);
+//     let n2 = Number(num2);
+//     if (isNaN(n1)||isNaN(n2)){
+//         return("enter proper number")
+//     }
+//     return n1 + n2;
+// }
+// console.log(addNum(10, "abc"));//nan-> not a number
+
+// let username = "sai";
+// console.log(typeof(username));
+
+// let username1 = new String("sai"); 
+// console.log(typeof(username1));
+ 
+// let username = "  ganesh   ";
+// console.log(username.toUpperCase());
+
+// let username = "  ganesh   ";
+// console.log(username.length);
+
+// let username = "ganesh";
+// console.log(username.charAt());
+// console.log(username.indexOf('gnh'));
+
+// let user ="    name  jay   "
+// console.log(user.trim().length);
+
+
+// let username = "ganesh";
+// username = username.replace('e','i');
+// console.log(username); 
+
+// let user = "jay kumawat";
+// let hasPa = user.includes("pa");  
+// console.log(hasPa);  
+
+// let user="jay%kumawat%hari";
+// console.log(user.split('%'))
+
+
+
+// let user="saideep"
+// console.log (user.substring(4,5))
+// console.log (user.slice(4,5))
+
+let user="saideep"
+console.log (user.substring(-4,5))
+console.log (user.slice(-4,5))
